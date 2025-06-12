dfb5587bbc17398f2a6a65dc707a23d962852b78
Nabin Tiwari <96787565+tywarinabin@users.noreply.github.com>
2025-06-12 18:56:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -197,3 +197,32 @@ document.addEventListener('DOMContentLoaded', function() {
     arrowIcon.textContent = isExpanded ? '˅' : '›';
     this.innerHTML = isExpanded ? 'View Less <span id="arrow-icon">˅</span>' : 'View More <span id="arrow-icon">›</span>';
   });
+
+
+document.getElementById('contactForm').addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const data = {
+    name: document.getElementById('name').value.trim(),
+    email: document.getElementById('email').value.trim(),
+    subject: document.getElementById('subject').value.trim(),
+    message: document.getElementById('message').value.trim()
+  };
+
+  fetch('https://script.google.com/macros/s/AKfycbx3zy4YjOJp2qDmHhyCOY4qxPDwyt6zC2dOe37D9w96YBSU3WwSq9k8bt-x6gQT7eA/exec', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+  .then(response => response.json())
+  .then(result => {
+    alert('Message sent successfully!');
+    document.getElementById('contactForm').reset();
+  })
+  .catch(error => {
+    console.error('Error!', error.message);
+    alert('There was an error sending your message.');
+  });
+});
